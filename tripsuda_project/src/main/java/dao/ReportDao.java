02d3bea250aa4be7fa6817a25eef79120dd5e102
june <dlrkdwnszz@gmail.com>
2022-06-23 12:15:58 +0900

    reportdao 신고 목록 추가

--- tripsuda_project/src/main/java/dao/ReportDao.java
+++ tripsuda_project/src/main/java/dao/ReportDao.java
@@ -1,12 +1,18 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import db.JdbcUtil;
+import vo.Expert_commVo;
+import vo.ReportVo;
 
 public class ReportDao {
+	//신고하기
 	public int insert(int defendant, int plaint, String reason ) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -27,4 +33,45 @@ public class ReportDao {
 	}
 	
 }
+	//신고목록 조회
+	public ArrayList<ReportVo> reportList(){
+		Connection con = null;
+		PreparedStatement pstmt =null;
+		ResultSet rs = null;
+		try {
+			con = JdbcUtil.getCon();
+			
+			String sql = "select * from report ";
+			pstmt =con.prepareStatement(sql);
+			rs  = pstmt.executeQuery();
+			
+			ArrayList<ReportVo> list = new ArrayList<ReportVo>();
+			while(rs.next()) {
+				int rnum = rs.getInt("rnum");
+				int defendant = rs.getInt("defendant");
+				int plaintiff = rs.getInt("plaintiff");
+				String reason = rs.getString("reason");
+				Date rdate = rs.getDate("rdate");
+						
+				ReportVo vo = new ReportVo(rnum, defendant, plaintiff, reason, rdate);
+				list.add(vo);
+			}
+			return list;
+			
+		}catch (SQLException s) {
+			 s.printStackTrace();
+			 return null;
+		}finally {
+			JdbcUtil.close(con, pstmt, rs);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
+
