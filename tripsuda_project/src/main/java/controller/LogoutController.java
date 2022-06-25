b8d29f0a6669ac1b78ef9ec3d910eadf3698d80e
KIM <KIM@LAPTOP-NL3FRU99>
2022-06-26 01:08:40 +0900

    로그아웃 세션 수정

--- tripsuda_project/src/main/java/controller/LogoutController.java
+++ tripsuda_project/src/main/java/controller/LogoutController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutController
@@ -15,8 +16,9 @@ public class LogoutController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-			request.getSession().removeAttribute("sessionID");
-			
+			//request.getSession().removeAttribute("sessionID");
+		    HttpSession session = request.getSession();
+			session.invalidate();
 		//	request.getRequestDispatcher("/html&jsp/Menu.jsp").forward(request, response);
 	        response.sendRedirect(request.getContextPath()+"/community");  	//kj 624수정 로그아웃시 main페이지
 	
