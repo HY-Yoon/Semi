b5887b4316907e274fcedfc0b2c3a1a24c121b11
Hye-In Yoon <heain1001@naver.com>
2022-06-26 17:40:12 +0900

    여행후기게시판 검색기능 수정(중간저장)

--- tripsuda_project/src/main/webapp/js/board_review/default.js
+++ tripsuda_project/src/main/webapp/js/board_review/default.js
@@ -4,5 +4,8 @@
 window.onload = function(){
 	document.getElementsByClassName("bot_navi active")[0].className ="bot_navi";
 	document.getElementsByClassName("bot_navi")[3].className ="bot_navi active";
+	//header 검색창 placeholder설정
+	let search = document.getElementById("search");
+	search.setAttribute("placeholder","여행후기를 찾아보세요!");
 }
  
